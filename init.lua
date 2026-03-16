9a6249fa75bae35d2f8a68606cbe28d6e31c0ad4
Logan Day <logannday@gmail.com>
2026-03-15 21:21:24 -0700

    some changes

--- init.lua
+++ init.lua
@@ -12,27 +12,29 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 vim.pack.add({
-	"https//github.com/neovim/nvim-lspconfig.git",
+	"https://github.com/nvim-lualine/lualine.nvim.git",
+	"https://github.com/neovim/nvim-lspconfig.git",
 	"https://github.com/nvim-treesitter/nvim-treesitter.git",
-	"https://github.com/echasnovski/mini.pick.git",
-	"https://github.com/vague2k/vague.nvim.git",
+	"https://github.com/ibhagwan/fzf-lua.git",
 	"https://github.com/Saghen/blink.cmp.git",
 	"https://github.com/stevearc/oil.nvim.git",
-	"https://github.com/nvim-lualine/lualine.nvim.git",
+	"https://github.com/ibhagwan/fzf-lua.git",
+	"https://github.com/catppuccin/nvim.git",
 })
 
-require('lualine').setup()
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<leader>t", "<CMD>e ~/.todos.md<CR>")
 
 -- Pickers --
-local mp = require("mini.pick")
-mp.setup()
-vim.keymap.set("n", "<C-p>", mp.builtin.files)
-vim.keymap.set("n", "<leader>h", mp.builtin.help)
-vim.keymap.set("n", "<leader>h", function() mp.builtin.files(nil, { source = { cwd = '../foo/bar' } }) end)
+local fzf = require("fzf-lua")
+fzf.setup()
+vim.keymap.set("n", "<C-p>", fzf.files)
+vim.keymap.set("n", "<leader>f", fzf.files)
+vim.keymap.set("n", "<leader>h", fzf.helptags)
+vim.keymap.set("n", "<leader>c", fzf.colorschemes)
+vim.keymap.set("n", "<leader>g", fzf.live_grep)
 
 -- LSP --
 vim.lsp.enable({ 'clangd', 'lua_ls', 'pyright', 'emmet_ls', 'svelte', 'tinymist', 'rust_analyzer' })
@@ -44,15 +46,22 @@ require("blink.cmp").setup {
 		keymap = { preset = "super-tab" }}
 
 -- TreeSitter --
-require 'nvim-treesitter.configs'.setup {
-	highlight = { enable = true, },
-	auto_install = true,
-}
+require'nvim-treesitter'.setup {}
+require'nvim-treesitter'.install { 'lua', 'rust', 'javascript', 'python', 'svelte', 'typescript' }
 
--- Oil--
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'lua', 'rust', 'javascript', 'python', 'svelte', 'typescript' },
+  callback = function() vim.treesitter.start() end,
+})
+
+
+-- Oil --
 require('oil').setup()
 vim.keymap.set("n", "-", "<Cmd>Oil<CR>")
 
 -- Colorscheme --
-require("vague").setup({ transparent = true })
-vim.cmd.colorscheme("vague")
+require("catppuccin").setup {
+	transparent_background = true,
+}
+require 'lualine'.setup{}
+vim.cmd.colorscheme "catppuccin"
